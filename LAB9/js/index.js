52c515cffd95430cad6fcb5df58ea3a3a0f19c9e
shoniyahh1 <thomasshoniyah@gmail.com>
2025-11-24 20:38:41 +0000

    Added lab 9 files

---
+++ LAB9/js/index.js
@@ -0,0 +1,73 @@
+let currentNumber = '';
+let previousNumber= '';
+let operation = null;
+
+const previousNumberElement = document.getElementById('previous-number');
+const currentNumberElement = document.getElementById('current-number');
+const operationNumberElement = document.getElementById('operation');
+
+function calculateResult() {
+    if (previousNumber === '' || currentNumber === '' || operation === null) {
+        return;
+}
+
+let result;
+const prev = parseFloat(previousNumber);
+const curr = parseFloat(currentNumber);
+
+switch (operation) {
+    case '+':
+        result = prev + curr;
+        break;
+        case '-':
+        result = prev - curr;
+        break;
+        case '*':
+        result = prev * curr;
+        break;
+        case '/':
+        result = curr !== 0 ? prev / curr : 'Error';
+        break;
+        default:
+            return;
+    }
+    currentNumber = result.toString(); 
+    previousNumber = '';
+    operation = null;
+    updateDisplay();
+}
+
+function appendNumber(number) {
+    currentNumber += number.toString();
+    updateDisplay();
+}
+
+function setOperation(op) {
+    if (currentNumber == '') {
+        return;
+    }
+
+    if(previousNumber !== '') {
+        calculateResult();
+    }
+
+    operation = op;
+    previousNumber = currentNumber;
+    currentNumber = '';
+    updateDisplay();
+}
+
+function clearCalculator() {
+    currentNumber = '';
+    previousNumber = '';
+    operation = null;
+    updateDisplay();
+}
+
+function updateDisplay() {
+    previousNumberElement.textContent = previousNumber;
+    currentNumberElement.textContent = currentNumber || '0';
+    operationNumberElement.textContent = operation || '';
+}
+    
+
